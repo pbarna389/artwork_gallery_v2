ac3613bbeb46461f21b68f223cd8925dbb333b2c
PB <pbarna389@gmail.com>
2025-03-13 13:37:25 +0100

    01/setup - modified setup import order

--- src/testSetup.ts
+++ src/testSetup.ts
@@ -1,13 +1,19 @@
-import { cleanup } from '@testing-library/react'
+/* eslint-disable vitest/no-duplicate-hooks */
+/* eslint-disable vitest/require-top-level-describe */
+/* eslint-disable vitest/prefer-hooks-in-order */
+/* eslint-disable vitest/no-hooks */
 import * as matchers from '@testing-library/jest-dom/matchers'
+import { cleanup } from '@testing-library/react'
 
 import '@testing-library/jest-dom/vitest'
+
 import { serviceWorker } from '@services'
 
 expect.extend(matchers)
 
 afterEach(() => {
 	cleanup()
+	serviceWorker.resetHandlers()
 })
 
 // Start worker before all tests
