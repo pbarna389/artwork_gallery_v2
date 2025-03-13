3e37008155790244ae259265aa89174f04930888
PB <pbarna389@gmail.com>
2025-03-13 13:02:55 +0100

    01/setup - App.test.tsx fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 
 import App from './App'
 
-describe('App component tests', () => {
-	test('Should render the App component', async () => {
+describe('app component tests', () => {
+	it('should render the App component', async () => {
 		render(<App />)
 
 		const component = await screen.findByText('The App will be here')
