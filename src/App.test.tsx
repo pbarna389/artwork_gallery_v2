18a51dc59794ee16928541a8bb0d715c2f1d1870
PB <pbarna389@gmail.com>
2025-03-13 13:24:03 +0100

    01/setup - added react-router v7

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,8 @@ import App from './App'
 
 describe('app component tests', () => {
 	it('should render the App component', async () => {
+		expect.hasAssertions()
+
 		render(<App />)
 
 		const component = await screen.findByText('The App will be here')
