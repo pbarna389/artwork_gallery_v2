f50f1e89dd6f6b74ce342371b2c0724f912a4473
PB <pbarna389@gmail.com>
2025-03-14 12:55:01 +0100

    01/setup - added some example documentation

--- src/services/msw/handlers.ts
+++ src/services/msw/handlers.ts
@@ -1,5 +1,11 @@
 import { http, HttpResponse } from 'msw'
 
+/**
+ * @remarks
+ * This is the handler for outgoing requests for unit tests
+ * @returns an array with predefined get requests
+ */
+
 export const handlers = [
 	http.get('http://localhost:5173', () => {
 		return HttpResponse.json({
--- src/services/msw/worker.ts
+++ src/services/msw/worker.ts
@@ -2,4 +2,8 @@ import { setupServer } from 'msw/node'
 
 import { handlers } from './handlers'
 
+/**
+ * The serviceWorker for intercepting outgoing requests for unit tests
+ */
+
 export const serviceWorker = setupServer(...handlers)
